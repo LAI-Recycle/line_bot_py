fdc5effb6b17f6046511578777a5c3d784a01ad9
LAI <77723979+LAI-Recycle@users.noreply.github.com>
2024-02-22 12:01:10 +0800

    update handler

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 #app.py
-from flask import Flask, request, abort
-#from dotenv import load_dotenv
 import os
+import random 
 
-# 載入 .env 文件
+from flask import Flask, request, abort
+
+#載入.env
+#from dotenv import load_dotenv
 #load_dotenv()
 
 from linebot import (
@@ -18,7 +20,9 @@ from linebot.models import (
 
 app = Flask(__name__)
 
+# 確認 token
 line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
+# 確認 secret
 handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
 
 @app.route("/callback", methods=['POST'])
@@ -41,10 +45,20 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+    msg = []
+    if("運勢" | "吃") in event.message.text:
+        if "運勢" in event.message.text:
+            fortune = random.choice(['大凶', '凶', '末吉', '吉','中吉','大吉'])
+            msg.append(TextSendMessage(text=fortune))  
+        if "吃" in event.message.text:
+            eat = random.choice(['水餃', '小7', '火鍋', '炒飯','拉麵','陽春麵'])
+            msg.append(TextSendMessage(text=eat)) 
+    else:
+        msg.append(TextSendMessage(text=event.message.text))
+
+    line_bot_api.reply_message(event.reply_token, messages=msg)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
